85176e6916e0e417feba6b4ffc1797cb2c06579c
EsraaNasr92 <esraanasr706@gmail.com>
2024-05-21 16:04:09 +0300

    CSS: Fixed border

--- src/Pages/Homes/Users.jsx
+++ src/Pages/Homes/Users.jsx
@@ -1,14 +1,14 @@
 export default function User(){
     return(
-        <div className="grow--section">
+        <div className="users--section">
             <div className="border-image-top"></div>
-            <div className="grow--section__text">
+            <div className="users--section__text">
                 <h1>Your Users</h1>
                 <p>It takes no time at all to integrate Huddle with your app's authentication solution. This means, 
                 once signed in to your app, your users can start chatting immediately.
                 </p>
             </div>
-            <div className="grow--section__img">
+            <div className="users--section__img">
                 <img src="./img/illustration-your-users.svg" alt="your-users" />
             </div>
             <div className="border-image-bottom"></div>
